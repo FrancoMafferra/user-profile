cf4f8314c80e1430a53667a884dd2c4464027227
franco <francomafferra38@gmail.com>
2024-07-02 12:13:02 -0300

    start

---
+++ user-profile/src/app/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img src="/bart_simpson.jpg" alt="Profile Picture" className={styles.profileImage} />
+      <h1 className={styles.userName}>Nombre del Usuario</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ user-profile/src/app/page.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Header from './components/Header';
+
+const Home = () => {
+  return (
+    <div>
+        <title>Perfil de Usuario</title>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
